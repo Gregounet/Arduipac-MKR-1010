3bc19cd748b5e51602e9dd1ed1399e66ee1108de
Gregoire TELLIER <gregoire.tellier@gmail.com>
2024-04-09 12:06:53 +0200

    Ajout de couleurs

---
+++ include/arduipac_colors.h
@@ -0,0 +1,13 @@
+#ifndef ARDUIPAC_COLORS_H
+#define ARDUIPAC_COLORS_H
+
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+extern const PROGMEM struct colors_tag
+{
+    uint8_t o2_color;
+    uint16_t tft_color;
+} colors[]
+
+#endif /* (ARDUIPAC_COLORS_H) */
